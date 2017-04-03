06b1dec7bc24e153d788409768e2cd66d155752b
lamthuyvo <lamthuyvo@gmail.com>
2017-04-02 21:18:28 -0700

    added charts

---
+++ 2017/2017-04-adnetworks/js/chart2.js
@@ -0,0 +1,64 @@
+// Replace this Javascript snippet from JSFiddle starting here...
+
+
+$(document).ready(function () {
+
+  var categories2 = ['Kixer','BlueKai','RevContent','Quantcast','eXelate','Google Syndication','ScoreCard Research Beacon','DoubleClick Ad Exchange-Seller','Google Adsense','DoubleClick']
+
+
+
+
+  Highcharts.chart('container2', {
+    chart: {
+        type: 'bar'
+    },
+    title: {
+        text: ''
+    },
+    subtitle: {
+        text: ''
+    },
+    xAxis: {
+        categories: categories2,
+        reversed: false,
+        labels: {
+            step: 1
+        }
+    },
+    yAxis: {
+        title: {
+            text: null
+        },
+        labels: {
+            formatter: function () {
+                return this.value;
+            }
+        }
+    },
+
+    plotOptions: {
+        series: {
+            stacking: 'normal'
+        }
+    },
+
+    tooltip: {
+        formatter: function () {
+            return '<b>' + this.point.category + '</b><br/>' +
+                'Change in trackers since the election: ' + Highcharts.numberFormat(Math.abs(this.point.y), 0);
+        }
+    },
+
+    series: [{
+        name: 'Most prominent trackers across fake news sites',
+        color:'#7FCEC2',
+        data: [18,18,18,19,20,34,35,40,46,53]
+    }]
+  });
+
+
+
+
+});
+
+//... and end your pasting of the JavaScript snippet from JSFiddle here
---
+++ 2017/2017-04-adnetworks/js/chart3.js
@@ -0,0 +1,60 @@
+// Replace this Javascript snippet from JSFiddle starting here...
+
+
+$(document).ready(function () {
+
+  var categories3=['aol','bidgear','clickdealer','criteo.com','mantis','pixfuture','propel','teads','time2play-online.net banners','advertise.com','conversant','infolinks','time2play-online.net popunder','adblade','taboola','ad provider','feednetwork','earnify','mgid','content.ad','popunder','adsense','revcontent']
+
+
+
+  Highcharts.chart('container3', {
+    chart: {
+        type: 'bar'
+    },
+    title: {
+        text: ''
+    },
+    subtitle: {
+        text: ''
+    },
+    xAxis: {
+        categories: categories3,
+        reversed: false,
+        labels: {
+            step: 1
+        }
+    },
+    yAxis: {
+        title: {
+            text: null
+        },
+        labels: {
+            formatter: function () {
+                return this.value;
+            }
+        }
+    },
+
+    plotOptions: {
+        series: {
+            stacking: 'normal'
+        }
+    },
+
+    tooltip: {
+        formatter: function () {
+            return '<b>' + this.point.category + '</b><br/>' +
+                'Number of ads served across 60+ sites:' + Highcharts.numberFormat(Math.abs(this.point.y), 0);
+        }
+    },
+
+    series: [{
+        name: 'Most served ads',
+        color:'#7FCEC2',
+        data: [1,1,1,1,1,1,1,1,1,2,2,2,2,3,4,5,5,6,7,9,9,17,22]
+    }]
+  });
+
+});
+
+//... and end your pasting of the JavaScript snippet from JSFiddle here
