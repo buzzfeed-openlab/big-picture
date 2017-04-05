24b51382663f5f681f7893b30e5ed95bacbfd293
Lam Thuy Vo <lamthuyvo@Lams-MacBook-Pro.local>
2017-04-04 17:47:03 -0700

    added new chart

---
+++ 2017/2017-04-heartrate/js/theme.js
@@ -0,0 +1,44 @@
+
+// setting highcharts theme
+Highcharts.theme = {
+  // colors: ['#E93F33'],
+  chart: {
+    backgroundColor: null,
+    style: {
+      fontFamily: '"Proxima Nova", "Helvetica", sans-serif'
+    }
+  },
+  title: {
+    style: {
+      color: '#555',
+      font: '18px "Proxima Nova", "Helvetica", sans-serif',
+      align: 'left'
+    }
+  },
+  legend: {
+    itemStyle: {
+      font: '9pt "Proxima Nova", "Helvetica", sans-serif',
+      color: '#555'
+    }
+  },
+  lang: {
+    thousandsSep: ','
+  },
+  credits: {
+      enabled: false
+  },
+  exporting: {
+         enabled: false
+  },
+  plotOptions: {
+        series: {
+            marker: {
+                enabled: false
+            }
+        }
+  }
+};
+
+
+// Apply the theme
+Highcharts.setOptions(Highcharts.theme);
